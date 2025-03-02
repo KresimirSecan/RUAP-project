ce7ee46a0bd7450fe7a601440bbb35bce8f4bd9b
KresimirSecan <105883617+KresimirSecan@users.noreply.github.com>
2025-03-02 11:45:01 +0100

    Update server.js

--- ruap_site/server.js
+++ ruap_site/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const axios = require('axios');
 
 const app = express();
@@ -8,12 +9,15 @@ const PORT = process.env.PORT || 3000;
 const AZURE_API_URL = 'http://5b1e3590-5b4f-4266-b4d5-16f10730a19f.eastus2.azurecontainer.io/score'; 
 const API_KEY = 'RX7TPG224ofJPSzdZgGfHacrTbb2cGHy'; 
 
+app.use(express.static(__dirname));
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); 
-});
+    res.sendFile(path.join(__dirname, 'index.html'));  
+  });
+
 
 app.post('/proxy', async (req, res) => {
     try {
